3de47185a1c75eb8ff3f4b887017054d63283f92
Nicolas Sotelo <nicolassotelo@Nicolass-MacBook-Air.local>
2019-09-18 20:57:39 -0500

    Integracion de node+expres+mongoDB +react

---
+++ app-back/routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+//Ejemplo conexion mongoDB
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://admin:admin@proyectoweb-n33pf.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+
+
+/* GET home page. */
+router.get('/prueba', function(req, res, next) {
+
+  client.connect(err => 
+    {
+    client.db("Idioma").collection("Usuarios").find({}).toArray((err,data)=>
+  {
+    res.send(data)
+  }  
+    )
+    
+    client.close();
+  });
+  
+});
+
+module.exports = router;
