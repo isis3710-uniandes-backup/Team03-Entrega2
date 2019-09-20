8fc23d23e88eccd2c6a67548730b002f16ed7bf9
SEBASTIAN RODRIGUEZ BELTRAN <s.rodriguezb1@sis.virtual.uniandes.edu.co>
2019-09-20 18:51:08 -0500

    componentes front

---
+++ app-back/front/src/components/idiomaList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Idioma from './idioma';
+
+class IdiomaList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default IdiomaList;
